da2bc3e65f82cf62a555b257eb47fa80e365fdff
KACIMI Jugurtha <kjugurtha1711@gmail.com>
2020-10-05 21:34:22 +0100

    Adding table containing cases by country

--- src/App.js
+++ src/App.js
@@ -1,35 +1,41 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import {
   MenuItem,
   FormControl,
   Select,
   Card,
   CardContent,
-} from "@material-ui/core";
-import "./App.css";
-import InfoBox from "./InfoBox";
-import Map from "./Map";
+} from '@material-ui/core';
+import './App.css';
+import InfoBox from './InfoBox';
+import Map from './Map';
+import Table from './Table';
+import { sortData } from './util';
 
 function App() {
   const [countries, setCountries] = useState([]);
-  const [country, setCountry] = useState("worldwide");
+  const [country, setCountry] = useState('worldwide');
   const [countryInfo, setCountryInfo] = useState({});
+  const [tableData, setTableData] = useState([]);
 
   useEffect(() => {
-    fetch("https://disease.sh/v3/covid-19/all")
+    fetch('https://disease.sh/v3/covid-19/all')
       .then((response) => response.json())
       .then((data) => setCountryInfo(data));
   }, []);
 
   useEffect(() => {
     const getCountriesData = async () => {
-      await fetch("https://disease.sh/v3/covid-19/countries")
+      await fetch('https://disease.sh/v3/covid-19/countries')
         .then((response) => response.json())
         .then((data) => {
           const countries = data.map((country) => ({
             name: country.country,
             value: country.countryInfo.iso2,
           }));
+
+          const sortedData = sortData(data);
+          setTableData(sortedData);
           setCountries(countries);
         });
     };
@@ -41,8 +47,8 @@ function App() {
     setCountry(countryCode);
 
     const url =
-      countryCode === "worldwide"
-        ? "https://disease.sh/v3/covid-19/all"
+      countryCode === 'worldwide'
+        ? 'https://disease.sh/v3/covid-19/all'
         : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
 
     await fetch(url)
@@ -97,6 +103,7 @@ function App() {
       <Card className="app__right">
         <CardContent>
           <h3>Live cases by country</h3>
+          <Table countries={tableData} />
           <h3>Worldwide new cases</h3>
         </CardContent>
       </Card>
