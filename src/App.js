1d655c2bf74816ccf209d63efe7b12db890d536a
KACIMI Jugurtha <kjugurtha1711@gmail.com>
2020-10-05 22:46:25 +0100

    Adding chart for new cases

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import InfoBox from './InfoBox';
 import Map from './Map';
 import Table from './Table';
 import { sortData } from './util';
+import LineGraph from './LineGraph';
 
 function App() {
   const [countries, setCountries] = useState([]);
@@ -105,6 +106,7 @@ function App() {
           <h3>Live cases by country</h3>
           <Table countries={tableData} />
           <h3>Worldwide new cases</h3>
+          <LineGraph />
         </CardContent>
       </Card>
     </div>
